e2a87444f12e933e9a2039de87bb7e89ba34a9fe
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-16 16:45:39 -0300

    feat: Requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,20 @@
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import pandas as pd
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        data = pd.read_csv(source_path)
+
+        for ingredients in data["dish"].unique():
+            diference = data[data["dish"] == ingredients]
+            price = diference["price"].iloc[0]
+            new = Dish(ingredients, price)
+            ingredient = diference[["ingredient", "recipe_amount"]].values.tolist()
+            for i1, i2 in ingredient:
+                ingrendient_result = Ingredient(i1)
+                new.add_ingredient_dependency(ingrendient_result, i2)
+            self.dishes.add(new)
--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,33 +1,36 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
 from src.models.ingredient import (
     Ingredient,
-    Restriction,
 )
 import pytest
 
 
 # Req 2
 def test_dish():
-    dish = Dish("pizza", 50.0)
-    dish2 = Dish("shimeji", 14.0)
-    dish3 = Dish("pizza", 50.0)
+    dish = Dish("Pizza", 15.99)
+    assert dish.name == "Pizza"
+    assert dish.__repr__() == "Dish('Pizza', R$15.99)"
 
-    assert dish.name == 'pizza'
-    assert dish.price == 50.0
+    dish2 = Dish("Pizza", 15.99)
+    assert dish == dish2
+    assert dish.__hash__() == dish2.__hash__()
 
-    assert dish != dish2
-    assert dish == dish3
+    dish3 = Dish("Sushi", 12.99)
+    assert dish != dish3
+    assert dish.__hash__() != dish3.__hash__()
 
-    assert hash(dish) != hash(dish2)
-    assert hash(dish) == hash(dish3)
+    with pytest.raises(TypeError):
+        Dish("name", "price")
+    with pytest.raises(ValueError):
+        Dish("name", 0)
 
-    ingredient = Ingredient("queijo gorgonzola")
-    dish.add_ingredient_dependency(ingredient, 200)
-    assert ingredient in dish.get_ingredients()
-
+    dish.add_ingredient_dependency(Ingredient("Tomate"), 3)
+    assert dish.recipe.get(Ingredient("Tomate")) == 3
     assert dish.get_restrictions() == set()
 
-    assert str(dish) == "Dish('pizza', R$50.00)"
-
-    with pytest.raises(ValueError):
-        Dish("pizza", -50)
+    dish.add_ingredient_dependency(Ingredient("Queijo"), 2)
+    assert dish.get_ingredients() == {
+        Ingredient("Tomate"),
+        Ingredient("Queijo")
+    }
+    assert dish.get_restrictions() == set()
