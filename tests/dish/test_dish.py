b30ac9f08ec9790fe0510f0749c97ad3cbd80961
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-16 16:09:54 -0300

    test: Requisito 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,33 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish = Dish("pizza", 50.0)
+    dish2 = Dish("shimeji", 14.0)
+    dish3 = Dish("pizza", 50.0)
+
+    assert dish.name == 'pizza'
+    assert dish.price == 50.0
+
+    assert dish != dish2
+    assert dish == dish3
+
+    assert hash(dish) != hash(dish2)
+    assert hash(dish) == hash(dish3)
+
+    ingredient = Ingredient("queijo gorgonzola")
+    dish.add_ingredient_dependency(ingredient, 200)
+    assert ingredient in dish.get_ingredients()
+
+    assert dish.get_restrictions() == set()
+
+    assert str(dish) == "Dish('pizza', R$50.00)"
+
+    with pytest.raises(ValueError):
+        Dish("pizza", -50)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,22 +1,21 @@
 from src.models.ingredient import (
     Ingredient,
     Restriction,
-    )  # noqa: F401, E261, E501
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    first_ingredient = Ingredient("Shiitake")
-    second_ingredient = Ingredient("Shiitake")
-    third_ingredient = Ingredient("Shimeji")
-
-    assert first_ingredient.name == "Shiitake"
+    first_ingredient = Ingredient("queijo gorgonzola")
+    second_ingredient = Ingredient("queijo gorgonzola")
+    third_ingredient = Ingredient("queijo provolone")
+    assert first_ingredient.name == "queijo gorgonzola"
     assert first_ingredient.restrictions == {
         Restriction.LACTOSE,
         Restriction.ANIMAL_DERIVED,
     }
 
-    assert repr(first_ingredient) == "Ingredient('Shiitake')"
+    assert repr(first_ingredient) == "Ingredient('queijo gorgonzola')"
 
     assert first_ingredient != third_ingredient
     assert first_ingredient == second_ingredient
