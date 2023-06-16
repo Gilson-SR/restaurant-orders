980a5c6455877e27dc431b1080d515aa14484692
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-16 15:23:40 -0300

    feat: Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,24 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+    )  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    first_ingredient = Ingredient("Shiitake")
+    second_ingredient = Ingredient("Shiitake")
+    third_ingredient = Ingredient("Shimeji")
+
+    assert first_ingredient.name == "Shiitake"
+    assert first_ingredient.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert first_ingredient.__repr__() == "Ingredient('Shiitake')"
+
+    assert first_ingredient == second_ingredient
+    assert first_ingredient != third_ingredient
+    assert hash(first_ingredient) == hash(second_ingredient)
+    assert hash(first_ingredient) != hash(third_ingredient)
